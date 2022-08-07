47684845cc9ef3b4a845c6f65898685bd694fa1d
Л Сергей <ragioniere1980@gmail.com>
2022-08-07 09:33:49 +0300

    modelView

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -7,18 +7,25 @@ import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     var count = 0
+    private lateinit var viewModel:MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var textView = findViewById<TextView>(R.id.tvCount)
-        var button = findViewById<Button>(R.id.btnCount)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        val textView = findViewById<TextView>(R.id.tvCount)
+        val button = findViewById<Button>(R.id.btnCount)
+
+        textView.text = viewModel.count.toString()
 
         button.setOnClickListener {
-            ++count
-            textView.text = count.toString()
+//            ++count
+//            textView.text = count.toString()
+            viewModel.updateCount()
+            textView.text = viewModel.count.toString()
         }
 
     }
